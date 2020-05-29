5d56f07e0b8c0765b9ce5e0397b0d5903bbdc3ed
X-LINE <54470120+Djawed-Hammadi@users.noreply.github.com>
2020-05-29 17:50:01 +0200

    Update meta.sh

--- tools/meta.sh
+++ tools/meta.sh
@@ -15,4 +15,18 @@ elif [[ $toz != "" ]];then
 printf "\n"
 printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($toz)\n\n"
 fi
-rm -r class.txt resources.arsc AndroidManifest.xml metasploit.apk 
+toz_1=$(grep -a "http://" class.txt)
+if [[ $toz_1 == "" ]];then
+printf ""
+elif [[ $toz_1 != "" ]];then
+printf "\n"
+printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($toz_1)\n\n"
+fi
+toz_2=$(grep -a "https://" class.txt)
+if [[ $toz_2 == "" ]];then
+printf ""
+elif [[ $toz_2 != "" ]];then
+printf "\n"
+printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($toz_2)\n\n"
+fi
+rm -r class.txt resources.arsc AndroidManifest.xml metasploit.apk  
