1ab3cc936fee6aa7a645145fa76b35f263be7af6
X-LINE <54470120+Djawed-Hammadi@users.noreply.github.com>
2020-05-31 02:20:02 +0200

    Update meta.sh

--- tools/meta.sh
+++ tools/meta.sh
@@ -7,26 +7,25 @@ printf "\033[0;32m[#] Start Building ...\n\n"
 apktool b xline -o metasploit.apk > /dev/null;
 printf "\n"
 unzip metasploit.apk > /dev/null;
-mv classes.dex class.txt
-toz=$(grep -a "tcp://" class.txt)
+toz=$(grep -a "tcp://" classes.dex | cut -d '/' -f2- | tr -d '\0' |tr -d "/,")
 if [[ $toz == "" ]];then
 printf ""
 elif [[ $toz != "" ]];then
 printf "\n"
 printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($toz)\n\n"
 fi
-http=$(grep -a "http://" class.txt)
+http=$(grep -a "http://" classes.dex | cut -d '/' -f2- | tr -d '\0' |tr -d "/,")
 if [[ $http == "" ]];then
 printf ""
 elif [[ $http != "" ]];then
 printf "\n"
 printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($http)\n\n"
 fi
-https=$(grep -a "https://" class.txt)
+https=$(grep -a "https://" classes.dex | cut -d '/' -f2- | tr -d '\0' |tr -d "/,")
 if [[ $https == "" ]];then
 printf ""
 elif [[ $https != "" ]];then
 printf "\n"
 printf "\033[1;33mHOST_AND_PORT \033[1;37m-->\033[0;32m Found : \033[1;37m($https)\n\n"
 fi
-rm -r class.txt resources.arsc AndroidManifest.xml metasploit.apk  
+rm -r classes.dex resources.arsc AndroidManifest.xml metasploit.apk  
